1182735212bd2055b7059f3c20e02f03e60949dd
Sridhar <sridhar6261@gmail.com>
2022-11-26 00:44:58 +0530

    adding ML notes

--- 19_machinelearning_datascience/1_intro_to_ml_and_how_it_works.py
+++ 19_machinelearning_datascience/1_intro_to_ml_and_how_it_works.py
@@ -24,3 +24,52 @@ often the role of a data science and a machine learning expert are quite overlap
 don't even have a clear distinction between what is a machine learning expert and a data science expert.
 The field of data science simply means analyzing data, looking at data, and then doing something with it, usually some
 sort of a business goal. So when we talk about machine learning, there's a lot of overlap with data science.'''
+
+'''
+what is Machine learning ?
+
+in useful functions we create, we give the input and we code the function in such a way that we expect some output.
+
+In Machine Learning, we give input and output and we expect the functions created out of it.. those are called 
+Algorithms or models or brains etc.. Disadvantage of this is that these algos gets complex and complex over the time.  
+'''
+
+'''
+History of data : https://www.udemy.com/course/complete-python-developer-zero-to-mastery/learn/lecture/16361652#notes
+
+spreadsheets > SQL(RDBMS) > BigData(noSQL, mongoDB) > MachineLearning
+
+Because at some point we have so much data that as humans we can't just look like we did at spreadsheets and look at 
+columns and rows and make business decisions. I mean, we still could, but then we'd be wasting all this data that we've 
+been getting over the years. So companies like Facebook and Google that collect massive amounts of data every single day
+are turning to things like machine learning so that instead of humans looking at the data and trying to figure things
+out, we give this data to machines so that they're better able, even better than humans to make business decisions.
+And this idea of machine learning really came to be because of this growth in data that we received from businesses, as 
+well as the improvements in CPUs, GPUs, that is, graphical processing units and computer advancements. So using the 
+massive amounts of data and massive improvements in computation, we can use these machines to give them this big data 
+and make decision for us, just like we used to with spreadsheets. Now, this is a simplified version of how we got here, 
+but I hope it gives you a reason as to why businesses like this idea of machine learning. '''
+
+'''
+What is ML?
+But at the end of the day, the only reason that we care about machine learning is that we're able to
+use machines to predict results based on incoming data.
+
+ML categories:
+supervised - input is already classified like already having been stored in CSV files.
+Unsupervised - clusters of data
+Reinforcement - teaching machines thru trial and error.
+
+'''
+
+'''
+Machine Learning 101:
+steps or procedure of ML analyst or expert work:
+1. import the data
+2. clean the data
+3. split the data into training set or test set. eg: out of 100 - 80 training sets / 20 test sets
+4. create a model. Most probably you import some algorithm that is present already in existing modules.
+5. check or test the output using test set.
+6. Improve
+ 
+Difficult part is getting data.'''
