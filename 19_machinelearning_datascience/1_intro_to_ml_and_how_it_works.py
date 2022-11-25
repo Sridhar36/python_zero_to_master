8c124b547dee67fde246214f546694de85be325c
Sridhar <sridhar6261@gmail.com>
2022-11-25 22:18:42 +0530

    updating git and web dev notes

---
+++ 19_machinelearning_datascience/1_intro_to_ml_and_how_it_works.py
@@ -0,0 +1,26 @@
+'''
+You see, it all starts with A.I. or artificial intelligence, which simply means a human intelligence exhibited by
+machines. An A.I. is, well, a machine that acts like a human. And currently in our industry we have something called
+narrow A.I. that is, machines can be just as good or even better than humans at a specific task.
+For example, detecting heart disease from images or at the game of go or even video games.
+But narrow A.I. that we have currently simply means those machines can only do one thing really well.
+They can't be like humans and have multiple abilities.
+That's called General A.I. and it's something that we're very, very far away from.
+
+
+ML:
+Now machine learning is a subset of AI, and machine learning is an approach to try and achieve artificial intelligence
+through systems that can find patterns in a set of data. And actually, Stanford University describes machine learning as
+the science of getting computers to act without being explicitly programmed.
+That is, getting machines to do things without us specifically saying, Do this, then do that, then do this.
+
+DL:deep learning or deep neural networks is just one of the techniques for implementing machine learning.
+For now, you can just think of it as a type of algorithm.
+But then we have this thing of data science, and often the role of a data science and a machine learning
+
+
+DS:
+often the role of a data science and a machine learning expert are quite overlapping, and most job descriptions actually
+don't even have a clear distinction between what is a machine learning expert and a data science expert.
+The field of data science simply means analyzing data, looking at data, and then doing something with it, usually some
+sort of a business goal. So when we talk about machine learning, there's a lot of overlap with data science.'''
