644a4b8f521744cef439be2e0ec0841dd6a2628d
Sridhar <sridhar6261@gmail.com>
2022-12-07 00:11:27 +0530

    updtaes

--- 7_advanced_python_4_error_handling/__init__.py
+++ 7_advanced_python_4_error_handling/__init__.py
@@ -5,4 +5,16 @@ built in exceptions list: https://docs.python.org/3/library/exceptions.html
 We can directly mention these in the except
 
 raise can be used when the execution needs to be stopped.
+
+------
+
+when you do not want your program to fail even when an error occurs and instead raise an
+exception you can use try, catch, finally or exception handling.
+
+try: when gets the error and sends control to except block
+except: same like catch in Java. we handle error here.
+
+eg: you are expecting some pop up in your automation script and you did not find it
+In that case you need not fail the test and wants to proceed.. you can use try catch block here.
+
 '''
