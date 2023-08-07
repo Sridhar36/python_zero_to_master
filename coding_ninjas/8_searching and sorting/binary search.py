a6224557262b49661fd0b4ee55c93cf8b04d40fd
Sridhar <sridhar6261@gmail.com>
2023-08-07 21:17:00 +0530

    updating

---
+++ coding_ninjas/8_searching and sorting/binary search.py
@@ -0,0 +1,12 @@
+'''
+
+binary search: the basic idea behind binary search is to divide the search space in half at each step until the target
+value is found or the search space becomes empty.
+
+a= [1,2,3,4,5,6,7]
+value_to_search(x) = 5
+
+1. sort array
+2.
+
+'''
