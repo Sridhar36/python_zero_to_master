42d3eff77386d97eca1027c93b5832060705863e
Sridhar <sridhar6261@gmail.com>
2023-09-14 00:12:07 +0530

    new add-ons.

--- 8_advanced_python_5_genarators_yield/genarators_intro_1.py
+++ 8_advanced_python_5_genarators_yield/genarators_intro_1.py
@@ -10,4 +10,12 @@ we create generators in Python using the range and yeild keyword
 
 yeild
 
+generators allow us generate a value..
+
+A generator is a special type of thing in Python that allows us to use a special keyword called yield
+and it can pause and resume functions.
+
+for example you need a list of 100000 numbers and use same. If you create this list and try use.. Then we need to
+create a list that has 100000 values then we need to access.. this is time taking and memory in-efficient
+Better way to do it is to return one value from list at once using yield.
 '''
